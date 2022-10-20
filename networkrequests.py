c1884643b2b7a1e04f19a9a25d0cf203f10d29b7
Julian Kasimir <info@jolution.de>
2022-10-20 17:42:19 +0200

    Update networkrequests.py

--- networkrequests.py
+++ networkrequests.py
@@ -1,26 +1,37 @@
 #!/usr/bin/python
 #
-# Network Requests search Python Script v0.0.1 by jolution
-# Description: If you enter a sitemap (without another sitemap below it) and the base domain, each of the pages will be searched for non-domain accesses. Theoretically, you can see on which Wordpress subpage Google Fonts are used remotely. Attention, this is only a test script which is not intended for production use. I do not guarantee that you can find out everything with it. Use at your own risk. Alpha version.
+# Network Requests search Python Script v0.0.2 by jolution
+# Description: If you enter a sitemap (without another sitemap below it), each of the pages will be searched for non-domain accesses. Theoretically, you can see on which Wordpress subpage Google Fonts are used remotely. Attention, this is only a test script which is not intended for production use. I do not guarantee that you can find out everything with it. Use at your own risk. Alpha version.
 # I am happy if you want to develop the script further.
 # For more information, see:
 # https://github.com/jolution/python-find-network-requests/
-# run from cmd with "python networkrequests.py"
-
+# run from cmd with "python networkrequests.py https://example.de/page-sitemap.xml"
+import sys
 import emoji
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse
 
 from playwright.sync_api import sync_playwright
 
-# Domain e.g. example.com without https or trailing slashes
-domain = 'example.com'
-
-# Attention: Do not enter a sitemap url which has other XML sitemaps inside. This is not programmed yet ;-)
-sitemap = 'https://example.com/post-sitemap.xml'
+sitemap = ''
 url = ''
 
+# Attention: Do not enter a sitemap url which has other XML sitemaps inside. This is not programmed yet ;-)
+if sys.argv[1].endswith("xml", 3):
+	sitemap = sys.argv[1]
+else:
+	url = sys.argv[1]
+
+# Domain e.g. example.com without https or trailing slashes
+domain = urlparse(sys.argv[1]).netloc
+
+test_list__googleFonts = ['fonts.googleapis.com', 'fonts.gstatic.com']
+test_list__googleCaptcha = ['www.google.com/recaptcha/', 'www.gstatic.com/recaptcha/']
+test_list__googleMaps = ['maps.gstatic.com']
 test_list__youtube = ['youtube.com', 'googlevideo.com', 'youtube-nocookie.com', 'yt3.ggpht.com', 'i.ytimg.com']
+test_list__vimeo = ['vimeocdn.com', 'vimeo.com']
+test_list__facebook = ['connect.facebook.net', 'www.facebook.com/tr']
 
 def urlsFromSitemap(sitemap):
     sitemap = requests.get(sitemap)
@@ -34,26 +45,36 @@ def urlsFromSitemap(sitemap):
 
 def checkIfExternal(data, page, prefix):
     if domain not in data.url:
-
-		# Google
-        if 'fonts.googleapis.com' or 'fonts.gstatic.com' in data.url:
+        if any(e in data.url for e in test_list__googleFonts):
             print (prefix, emoji.emojize(':collision:'),'Google Fonts find on', page.url, ':', data.url)
         elif 'googleapis.com' in data.url:
             print (prefix, emoji.emojize(':collision:'),'Google Apis find on', page.url, ':', data.url)
-        elif 'google.com' in data.url:
-            print (prefix, emoji.emojize(':collision:'),'Google Connection find on', page.url, ':', data.url)
-
-		# Other
-        elif 'connect.facebook.net' or 'www.facebook.com/tr' in data.url:
-            print (prefix, emoji.emojize(':clinking_glasses:'),'Facebook Pixel find on', page.url, ':', data.url)
+        elif 'paypalobjects.com' in data.url:
+            print (prefix, emoji.emojize(':collision:'),'PayPal Connection find on', page.url, ':', data.url)
+        elif 'google-analytics.com' in data.url:
+            print (prefix, emoji.emojize(':collision:'),'Google Analytics find on', page.url, ':', data.url)
         elif 's.w.org' in data.url:
             print (prefix, emoji.emojize(':thumbs_down:'),'wp emoji find on', page.url, ':', data.url)
         elif 'consentmanager.net' in data.url:
             print (prefix, emoji.emojize(':cookie:'),'consentmanager find on', page.url, ':', data.url)
         elif 'fontawesome.com' in data.url:
             print (prefix, emoji.emojize(':collision:'),'Fontawesome CDN find on', page.url, ':', data.url)
+        elif any(e in data.url for e in test_list__googleCaptcha):
+            print (prefix, emoji.emojize(':collision:'),'Google Recaptcha find on', page.url, ':', data.url)
+        elif any(e in data.url for e in test_list__googleMaps):
+            print (prefix, emoji.emojize(':collision:'),'Google Maps find on', page.url, ':', data.url)
+        elif 'gravatar.com' in data.url:
+            print (prefix, emoji.emojize(':collision:'),'Gravatar find on', page.url, ':', data.url)
+        elif 'hcaptcha.com' in data.url:
+            print (prefix, emoji.emojize(':collision:'),'Hcaptcha find on', page.url, ':', data.url)
+        elif any(e in data.url for e in test_list__facebook):
+            print (prefix, emoji.emojize(':clinking_glasses:'),'Facebook Pixel find on', page.url, ':', data.url)
         elif any(e in data.url for e in test_list__youtube):
             print (prefix, emoji.emojize(':collision:'),'Google YouTube find on', page.url, ':', data.url)
+        elif any(e in data.url for e in test_list__vimeo):
+            print (prefix, emoji.emojize(':collision:'),'Google YouTube find on', page.url, ':', data.url)
+        elif 'google.com' in data.url:
+            print (prefix, emoji.emojize(':collision:'),'Google Connection find on', page.url, ':', data.url)
         else:
             print (prefix, emoji.emojize(':thinking_face:'), 'Unkown', page.url, data.url)
     #else:
